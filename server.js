ae18f6d89388b34177da9d2aa2e9c1e8f07d7969
Sasha Jackson <drejackson@Sashas-MacBook-Pro.local>
2021-03-12 13:12:02 -0600

    added dist file and updated server

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('./dist/skjangular'));
+app.use(express.static('./dist/angularproject1'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/skjangular/'}),
+    res.sendFile('index.html', {root: 'dist/angularproject1/'}),
 );
 
 app.listen(process.env.PORT || 8080);
