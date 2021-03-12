ba1b2260ab5bb71241b3b0973b03ebd3867608c4
Sasha Jackson <drejackson@Sashas-MacBook-Pro.local>
2021-03-12 12:27:12 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"about", component: AboutComponent},
+  {path:"projects", component: ProjectsComponent},
+  {path:"blogs", component: BlogsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    BlogsComponent
   ],
   imports: [
     BrowserModule,
