d71b2efda66f29c22d57faac7df9f90948426503
Sasha Jackson <drejackson@Sashas-MacBook-Pro.local>
2021-03-12 13:27:55 -0600

    update procfile and server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT || 8080;
 
 app.use(express.static('./dist/angularproject1'));
 
@@ -8,4 +9,6 @@ app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/angularproject1/'}),
 );
 
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => {
+    console.log('listening on port: ', port);
+});
