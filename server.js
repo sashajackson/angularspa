ac690d8e3234fb29172446673fd3407fa1550db4
Sasha Jackson <drejackson@Sashas-MacBook-Pro.local>
2021-03-12 12:57:30 -0600

    fix path in server

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/skjangular'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/skjangular/'}),
 );
 
 app.listen(process.env.PORT || 8080);
